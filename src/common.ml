7fe521486d2a1b528337fd201d865901913be77b
Vlad Lopalo <vlad.lopalo@gmail.com>
2019-11-03 02:40:42 -0500

    Initial commit

---
+++ src/common.ml
@@ -0,0 +1,6 @@
+let check () =
+  let q = 44 in
+  if q == 33 then [] else [1; 2; 3]
+
+
+let message = "Foo message"
---
+++ src/main.ml
@@ -0,0 +1,8 @@
+
+let main () =
+  print_endline Sandbox.Common.message;
+  print_endline "Ololo!"
+
+let () = main ()
+
+
